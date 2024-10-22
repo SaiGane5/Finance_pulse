9a54771ca076eecfb4206f8b3e7e5ce5c8b23052
Imandi Sai Ganesh <128854913+SaiGane5@users.noreply.github.com>
2024-10-22 14:14:53 +0530

    Update Dashboard.tsx

--- src/pages/Dashboard.tsx
+++ src/pages/Dashboard.tsx
@@ -11,7 +11,7 @@ const Dashboard = () => {
   useEffect(() => {
     const fetchData = async () => {
       try {
-        const response = await axios.get('http://0.0.0.0:8000/api/cash-flow/AAPL');
+        const response = await axios.get('https://finance-pulse.onrender.com/api/cash-flow/AAPL');
         setCashFlow(response.data[0]);
       } catch (error) {
         console.error('Error fetching cash flow data:', error);
